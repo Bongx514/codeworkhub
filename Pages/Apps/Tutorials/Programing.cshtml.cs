e86f7aa224507ed99eb3e24ec56a2afcea79fa58
bongx514 <bonganithwala514@live.com>
2025-11-26 13:09:27 +0200

    adding tutorials

---
+++ Pages/Apps/Tutorials/Programing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Codeworkhub.Pages.Apps.Tutorials
+{
+    public class ProgramingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
